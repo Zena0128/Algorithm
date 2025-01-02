6f04d7829c329ddb67278567ba99fb7b677b5edd
Jung-A Lee <70526434+Zena0128@users.noreply.github.com>
2025-01-02 12:53:24 +0900

    [Silver II] Title: 랭킹전 대기열, Time: 156 ms, Memory: 16980 KB -BaekjoonHub

--- 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.java
+++ 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.java
@@ -1,25 +1,23 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] meta = br.readLine().split(" ");
-        int p = Integer.parseInt(meta[0]);
-        int m = Integer.parseInt(meta[1]);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int p = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
         List<LinkedHashMap<String, Integer>> rooms = new ArrayList<>();
-        
-        for (int i=0;i<p;i++) {
-            String[] temp = br.readLine().split(" ");
-            int l = Integer.parseInt(temp[0]);
-            String n = temp[1];
+
+        for (int i = 0; i < p; i++) {
+            st = new StringTokenizer(br.readLine());
+            int l = Integer.parseInt(st.nextToken());
+            String n = st.nextToken();
 
             boolean ok = false;
-            for (int j=0;j<rooms.size();j++) {
+            for (int j = 0; j < rooms.size(); j++) {
                 LinkedHashMap<String, Integer> nowRoom = rooms.get(j);
                 if (levelAvailable(l, nowRoom.values().iterator().next())) {
                     if (nowRoom.size() < m) {
@@ -27,30 +25,38 @@ public class Main {
                         ok = true;
                         break;
                     }
-                };
+                }
             }
 
             if (!ok) {
                 rooms.add(new LinkedHashMap<>());
-                rooms.get(rooms.size()-1).put(n, l);
+                rooms.get(rooms.size() - 1).put(n, l);
             }
         }
 
-        for (int i=0;i<rooms.size();i++) {
+        for (int i = 0; i < rooms.size(); i++) {
             LinkedHashMap<String, Integer> nowRoom = rooms.get(i);
             if (nowRoom.size() == m) {
-                System.out.println("Started!");
+                bw.write("Started!\n");
             } else {
-                System.out.println("Waiting!");
+                bw.write("Waiting!\n");
             }
             nowRoom.entrySet().stream()
                 .sorted(Map.Entry.comparingByKey())
-                .forEach(entry -> System.out.println(entry.getValue() + " " + entry.getKey()));
+                .forEach(entry -> {
+                    try {
+                        bw.write(entry.getValue() + " " + entry.getKey() + "\n");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
         }
+
+        bw.flush();
+        bw.close();
     }
 
     static boolean levelAvailable(int player, int room) {
-        return player <= room+10 && player >= room-10;
+        return player <= room + 10 && player >= room - 10;
     }
-
 }
