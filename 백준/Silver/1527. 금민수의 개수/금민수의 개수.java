b4a4e67059cb464e43646b48c0b50f6d35f177e3
Jung-A Lee <70526434+Zena0128@users.noreply.github.com>
2025-02-05 21:45:48 +0900

    [Silver I] Title: 금민수의 개수, Time: 180 ms, Memory: 17812 KB -BaekjoonHub

--- 백준/Silver/1527. 금민수의 개수/금민수의 개수.java
+++ 백준/Silver/1527. 금민수의 개수/금민수의 개수.java
@@ -2,6 +2,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/*
+ * 단순 노가다. 브루트포스
+ * 어차피 범위가 커봤자 백만이므로, java 정수 범위만 long으로 잘 신경쓰면
+ * 1~100만까지, 4와 7로만 이루어진 수들을 만들어서 a, b와 범위비교해 개수 계산하면 끗
+ */
+
 public class Main {
     static int ans;
     static int a;
@@ -13,6 +19,8 @@ public class Main {
         b = sc.nextInt();
         sc.close();
 
+        // 특정 자리수를 가지는 금민수들을 저장할 리스트
+        // ex. {4,7} -> {44, 47, 74, 77} -> {444, 447, 474, 477, 744, 747, 774, 777}
         list = new ArrayList<>();
         list.add((long)4);
         list.add((long)7);
@@ -27,9 +35,11 @@ public class Main {
         solve();
         System.out.println(ans);
     }
+    
+    // 
     static void solve() {
         int[] arr = {4, 7};
-        for (int i=0;i<9;i++) {
+        for (int i=0;i<8;i++) {
             // System.out.println(list);
             List<Long> tempList = new ArrayList<>();
             int size = list.size();
